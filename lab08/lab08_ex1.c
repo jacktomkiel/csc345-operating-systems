fa5838c3869d15d3986800c54823138ff160a360
jacktomkiel <tomkiej1@tcnj.edu>
2022-03-24 12:05:07 -0400

    added zip file

--- lab08/lab08_ex1.c
+++ lab08/lab08_ex1.c
@@ -5,23 +5,24 @@ Lab 8 Exercise 1 */
 #include <stdlib.h>
 #include <stdio.h>
 
+/* PAGE_SIZE = 4KB = (4)*(1)*(2^10) = 2^12 = 4096 bytes */
 #define PAGE_SIZE 4096
 
 int main(int argc, char** argv)
 {
-    if (argc != 2)
-    {
-        printf("Enter only 1 argument.\n");
-        return 1;
-    }
-    
-    unsigned int virtual_address =  (unsigned int)atoi(argv[1]);
-    int page = virtual_address / PAGE_SIZE;
-    int offset = virtual_address % PAGE_SIZE;
+    unsigned int addr;
+    unsigned int page;
+    unsigned int offset;
 
-    printf("The address %u is located at:\n", virtual_address);
-    printf("page number = %d\n", page);
-    printf("offeset = %d\n", offset);
+    addr =  atoi(argv[1]);
+    page = addr / PAGE_SIZE;
+    offset = addr % PAGE_SIZE;
+
+    printf("The address %u is located at:\n", addr);
+    printf("Page number = %u\n", page);
+    printf("Offset = %u\n", offset);
 
     return 0;
 }
+
+/* to zip file: >>> lab08 % zip -r lab08.zip . -x ".*" -x "__MACOSX" <<< */
