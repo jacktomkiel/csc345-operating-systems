c564646e72b0ff6550ef3bdb39c51a844bfdaf45
jacktomkiel <tomkiej1@tcnj.edu>
2022-04-21 10:59:06 -0400

    lab12 init

---
+++ lab12/lab12_ex1.c
@@ -0,0 +1,13 @@
+/** Jack Tomkiel
+CSC345-01
+Lab 12 Exercise 1 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    printf("hello, world!\n");
+
+    return 0;
+}
